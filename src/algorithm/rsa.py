fa2420d9a7eb62ba2121b81974607a161f222700
AhmedIbrahimAbdellatif <Ahmed.Ahmed99@eng-st.cu.edu.eg>
2022-05-08 21:56:10 +0200

    RSA algo - Refactor code

--- src/algorithm/rsa.py
+++ src/algorithm/rsa.py
@@ -12,21 +12,24 @@ class RSA:
         assert self.prime_min_length >= 2, "prime min length must be greater than or equal 2" 
         assert self.key_length > self.prime_min_length, "key length must be > prime min length" 
         self.prime_max_length = self.key_length - self.prime_min_length
-        assert self.prime_max_length > self.prime_min_length, "min length must be less than half the key length" 
+        assert self.prime_max_length > self.prime_min_length, "min length must be less than half the key length"
+        self.first_sample_size = math.floor(config["KEY_GENERATION"]["FIRST_SAMPLE_SIZE"])
+        self.middle_sample_size = math.floor(config["KEY_GENERATION"]["MIDDLE_SAMPLE_SIZE"])
+        self.third_sample_size = math.floor(config["KEY_GENERATION"]["THIRD_SAMPLE_SIZE"])
     
     def generate_key(self):
         smallest_prime = 2 ** (self.prime_min_length - 1)
         p_max = (2 ** self.prime_max_length) - 1
-        prime_candidates = RSAUtils.get_prime_candidates(self.key_length, smallest_prime, p_max)
-        method = UserInputUtils.get_prime_selection_mode(prime_candidates, "p & q")
+        method = UserInputUtils.get_selection_mode(np.array([]), "p & q")
         if method == "Randomly":
-            p, q = RSAUtils.get_random_p_q(self.key_length, smallest_prime, prime_candidates)
-        else:    
-            p, q = RSAUtils.get_p_q_from_user(self.key_length, smallest_prime, prime_candidates)
+            p, q = RSAUtils.get_random_p_q(self.key_length)
+        else:
+            prime_candidates = RSAUtils.get_prime_candidates(self.key_length, smallest_prime, p_max, self.first_sample_size, self.middle_sample_size, self.third_sample_size)
+            p, q = RSAUtils.get_p_q_from_user(self.key_length, smallest_prime, prime_candidates, self.first_sample_size, self.middle_sample_size, self.third_sample_size)
         n = p * q
         phi = (p - 1) * (q - 1)
         coprime_candidates = RSAUtils.get_coprime_candidates(phi)
-        method = UserInputUtils.get_prime_selection_mode(coprime_candidates, "e")
+        method = UserInputUtils.get_selection_mode(coprime_candidates, "e")
         if method == "Randomly":
             e = RSAUtils.get_random_e(coprime_candidates)
         else:    
--- src/algorithm/utils.py
+++ src/algorithm/utils.py
@@ -6,7 +6,7 @@ from progressbar import *
 import timeit
 
 class UserInputUtils:
-    def get_prime_selection_mode(candidates, parameters):
+    def get_selection_mode(candidates, parameters):
         question = [
             inquirer.List('mode',
                         message="There are " + str(np.asarray(candidates).size) + " candidates for " + parameters + " how do you want it/them to be choosen?",
@@ -25,47 +25,31 @@ class UserInputUtils:
         return inquirer.prompt(options)["parameter"]
 
 class RSAUtils:
-    def get_prime_candidates(key_length, prime_min, prime_max):
-        middle_range_start = 2 ** (math.floor(key_length / 2) - 2)
-        middle_range_end = (2 ** (math.ceil(key_length / 2) + 1)) - 1
-        middle_sample_size = min(30, (middle_range_end-middle_range_start+1))
-        first_sample_size = min(7, (middle_range_start-prime_min))
-        third_sample_size = min(7, (middle_range_start-prime_min))
-        
-        if(middle_range_end > prime_max):
-            # important for choosing value for q as p might be large enough to rule out all random choices of q
-            prime_candidates = RSAUtils.get_n_primes(prime_min, prime_max+1, min(20, (prime_max-prime_min+1)))
-        else:
-            first_sample = RSAUtils.get_n_primes(prime_min, middle_range_start-1, first_sample_size)
-            middle_sample = RSAUtils.get_n_primes(middle_range_start, middle_range_end, middle_sample_size)
-            third_sample = RSAUtils.get_n_primes(middle_range_end, prime_max, third_sample_size)
-            prime_candidates = first_sample + middle_sample + third_sample
-        return prime_candidates
-
+    def get_random_p_q(key_length):
+        start = 2 ** (math.floor(key_length / 2) - 2)
+        end = (2 ** (math.ceil(key_length / 2) + 1)) - 1
+        prime_candidates = RSAUtils.get_n_primes(start, end+1, 2)
+        while (len(prime_candidates) == 1 or (not((prime_candidates[0] != prime_candidates[1]) and ((prime_candidates[0]*prime_candidates[1]).bit_length() <= key_length)))):
+            second_candidates = RSAUtils.get_n_primes(start, end+1, 5)
+            for candidate in second_candidates:
+                if((prime_candidates[0] != candidate) and ((prime_candidates[0]*candidate).bit_length() <= key_length)):
+                    prime_candidates[1] = candidate
+        return prime_candidates[0], prime_candidates[1]
+    
     def get_n_primes(range_start, range_end, count):
         
         widgets = ['Generating primes: ', Percentage(), ' ', Bar(marker='0',left='[',right=']'),
            ' ', ETA(), ' ', FileTransferSpeed(unit="prime")]
         pbar = ProgressBar(widgets=widgets, maxval=count)
         pbar.start()
-
-        first_primes_list = np.asarray([2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-            31, 37, 41, 43, 47, 53, 59, 61, 67,
-            71, 73, 79, 83, 89, 97, 101, 103,
-            107, 109, 113, 127, 131, 137, 139,
-            149, 151, 157, 163, 167, 173, 179,
-            181, 191, 193, 197, 199, 211, 223,
-            227, 229, 233, 239, 241, 251, 257,
-            263, 269, 271, 277, 281, 283, 293,
-            307, 311, 313, 317, 331, 337, 347, 349])
         primes = set()
-        for prime in first_primes_list[(first_primes_list >= range_start) & (first_primes_list <= range_end)]:
+        for prime in RSAUtils.first_primes_list[(RSAUtils.first_primes_list >= range_start) & (RSAUtils.first_primes_list <= range_end)]:
             primes.add(int(prime))
         start = timeit.default_timer()
         while len(primes) < count:
             prime_candidate = random.randrange((range_start|1), range_end+1, 2)
-            is_probable_prime = RSAUtils.is_probable_prime(range_start, first_primes_list, prime_candidate)
-            if is_probable_prime and RSAUtils.is_miller_rabin_strong_prime(prime_candidate): 
+            is_probably_a_prime = RSAUtils.is_probably_a_prime(range_start, prime_candidate)
+            if is_probably_a_prime and RSAUtils.is_miller_rabin_strong_prime(prime_candidate): 
                 primes.add(prime_candidate)
                 if timeit.default_timer() - start > 5:
                     pbar.update(count)
@@ -75,29 +59,26 @@ class RSAUtils:
         pbar.finish()
         return list(primes)
 
-    def is_probable_prime(range_start, first_primes_list, prime_candidate):
-        if(first_primes_list[(prime_candidate % first_primes_list == 0) & (first_primes_list**2 <= prime_candidate)].size > 0):
+    def is_probably_a_prime(range_start, prime_candidate):
+        if(RSAUtils.first_primes_list[(prime_candidate % RSAUtils.first_primes_list == 0) & (RSAUtils.first_primes_list**2 <= prime_candidate)].size > 0):
             return False
         return True
-
 
     def is_miller_rabin_strong_prime(prime_candidate):
         num_factorization_trials = 0
         factor = prime_candidate-1
-
         while factor % 2 == 0:
             factor >>= 1
             num_factorization_trials += 1
-
         num_miller_tests = 20
         for _ in range(num_miller_tests):
             base = random.randrange(2,
                         prime_candidate)
-            if RSAUtils.isFactorized(prime_candidate, base, factor, num_factorization_trials):
+            if RSAUtils.is_factorized(prime_candidate, base, factor, num_factorization_trials):
                 return False
         return True
     
-    def isFactorized(prime_candidate, base, factor, num_factorization_trials):
+    def is_factorized(prime_candidate, base, factor, num_factorization_trials):
             if pow(base, factor,
                 prime_candidate) == 1:
                 return False
@@ -107,8 +88,23 @@ class RSAUtils:
                     return False
             return True
 
+    def get_prime_candidates(key_length, prime_min, prime_max, default_first_sample_size, default_middle_sample_size, default_third_sample_size):
+        middle_range_start = 2 ** (math.floor(key_length / 2) - 2)
+        middle_range_end = (2 ** (math.ceil(key_length / 2) + 1)) - 1
+        middle_sample_size = min(default_middle_sample_size, (middle_range_end-middle_range_start+1))
+        if(middle_range_end > prime_max):
+            # important for choosing value for q as p might be large enough to rule out all random choices of q
+            prime_candidates = RSAUtils.get_n_primes(prime_min, prime_max+1, min(middle_sample_size, (prime_max-prime_min+1)))
+        else:
+            first_sample_size = min(default_first_sample_size, (middle_range_start-prime_min))
+            third_sample_size = min(default_third_sample_size, (middle_range_start-prime_min))
+            first_sample = RSAUtils.get_n_primes(prime_min, middle_range_start-1, first_sample_size)
+            middle_sample = RSAUtils.get_n_primes(middle_range_start, middle_range_end, middle_sample_size)
+            third_sample = RSAUtils.get_n_primes(middle_range_end, prime_max, third_sample_size)
+            prime_candidates = first_sample + middle_sample + third_sample
+        return prime_candidates
 
-    def get_p_q_from_user(key_length, smallest_prime, prime_candidates):
+    def get_p_q_from_user(key_length, smallest_prime, prime_candidates, first_sample_size, middle_sample_size, third_sample_size):
         p =  UserInputUtils.get_value_from_user("p", prime_candidates)
         q_max = RSAUtils.get_2nd_prime_max(key_length, prime_candidates, p)
         q_candidates = []
@@ -117,7 +113,7 @@ class RSAUtils:
         assert(set(q_candidates).issubset(prime_candidates))
         if len(q_candidates) == 0 :
             print("regenerating")
-            q_candidates = RSAUtils.get_prime_candidates(key_length, smallest_prime, q_max)
+            q_candidates = RSAUtils.get_prime_candidates(key_length, smallest_prime, q_max, first_sample_size, middle_sample_size, third_sample_size)
         q =  UserInputUtils.get_value_from_user("q", q_candidates)
         return p,q
     
@@ -133,25 +129,6 @@ class RSAUtils:
                 q_max = max_candidate
                 in_list = True
         return q_max
-
-    def get_random_p_q(key_length, smallest_prime, prime_candidates):
-        p_min = 2 ** (math.ceil(key_length / 2))
-        p_max = (2 ** (math.ceil(key_length / 2) + 1)) - 1
-        rand_idx=0
-        while prime_candidates[rand_idx] < p_min or  prime_candidates[rand_idx] > p_max: rand_idx = int(random.random() * len(prime_candidates))
-        p = prime_candidates[rand_idx]
-        q_max = RSAUtils.get_2nd_prime_max(key_length, prime_candidates, p)
-        q_min = 2 ** (math.floor(key_length / 2) - 2)
-        q_min = smallest_prime if q_min >= q_max else q_min
-        q_candidates = np.asarray(prime_candidates, dtype=object)
-        q_candidates = list(q_candidates[((q_candidates != p) & (q_candidates >= smallest_prime) & (q_candidates <= q_max))])
-        assert(set(q_candidates).issubset(prime_candidates))
-        if len(q_candidates) == 0 :
-            print("regenerating")
-            q_candidates = RSAUtils.get_prime_candidates(key_length, q_min, q_max)
-        rand_idx=int(random.random() * len(q_candidates))
-        q = q_candidates[rand_idx]
-        return p,q
     
     def get_coprime_candidates(phi):
         assert(isinstance(phi, int))
@@ -166,7 +143,6 @@ class RSAUtils:
         n = n1 | n2
         if n == n1 or n == n2:
             return n
-        
         x = n1
         y = n2
         while 1:
@@ -179,3 +155,9 @@ class RSAUtils:
     def get_random_e(coprime_candidates):
         rand_idx = int(random.random() * len(coprime_candidates))
         return coprime_candidates[rand_idx]
+    
+    
+    first_primes_list = np.asarray([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+            71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
+            181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,263, 269, 271, 277, 281, 283, 293,
+            307, 311, 313, 317, 331, 337, 347, 349])
